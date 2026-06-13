b472eed4309506e727b3ed772f66b36359b3f773
erebo <rahmanmahadi177153@gmail.com>
2026-06-13 09:29:06 +0600

    Objects and their memory location

---
+++ Basics/student.java
@@ -0,0 +1,5 @@
+public class student {
+    public String name; // Instance Variable 
+    public String id; // Instance Variable
+}
+// Instance variable ke object create kora chara access kora jay na !
--- Basics/tester.java
+++ Basics/tester.java
@@ -1,6 +1,32 @@
-public class tester
-{
-    public static void main(String[] args){
-        System.out.println("Hello World !");
+// public class tester
+// {
+//     public static void main(String[] args){
+//         System.out.println("Hello World !");
+//     }
+// }
+
+public class tester{
+    public static void main (String[] args){
+        // Classname variable = new Classname()
+        student s1 = new student (); // Object 1
+        student s2 = new student (); // Object 2
+        student s3 = new student();
+        int x = 5;
+        s1.name = "Mahadi";
+        s1.id = "008";
+        System.out.println(5);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1.id);
+        System.out.println(s2.name);
+        s3.name = "Obito";
+        s3.id = "007";
+        System.out.println(s3.name+" "+s3.id);
+        // Referencing Copy : 
+        s1 = s3;
+        s1.name = "Billu";
+        System.out.println(s3.name);
     }
+
+
 }
